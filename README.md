32bb8a4139ce660660090b206d5b240e5321558e
DianaRatnikova <Diana_r@bk.ru>
2022-09-20 21:26:39 +0300

    Eliminated extra nesting

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cities_console_game
 Game "Cities", where it's necessary to write a city name starting with the last letter of previous word
+
+Игра в города. 
+Запуск:
+из каталога cities_console_game: $ py cities_console_game.py
+
+
+Опции:
+Пользователю даётся три попытки на ответ.
+
+Возможные реакции программы:
+- "Нет такого города"
+- "Этот город начинается не на ту букву"
+- "На нужную букву нет вариантов, вы проиграли"
+- "Вы выиграли" 
+
+Если город заканчивается на ь, ъ, ы - берётся предпоследняя буква
--- cities_console_game.py
+++ cities_console_game.py
@@ -1,27 +1,17 @@
 __author__ = 'DIANA'
 
-'''
-Написать скрипт для игры в города
-
-принять на вход название города
-определить его последнюю букву
-найти в словаре город на эту букву, вывести его на экран, удалить это солово
-если не находит - удалить
-
-'''
 
 import random
-from dictionary import make_letters_low, show_last_letter, 
-                       make_list_with_first_letter
-import dictionary
+from cities_database import make_letters_low, show_last_letter, make_list_with_first_letter
+import cities_database
 
 
-def city_from_user(list_of_cities_names, attempts, first_letter=''):
+def handle_city_from_user(list_of_cities_names, attempts, first_letter=''):
     cities_with_first_letter = []
 
     if first_letter:
         cities_with_first_letter = make_list_with_first_letter(list_of_cities_names, first_letter)
-        if len(cities_with_first_letter) == 0:
+        if not cities_with_first_letter:
             print(f"Вы проиграли, нет больше городов на букву '{first_letter.capitalize()}' :)")
             return False
 
@@ -29,11 +19,8 @@ def city_from_user(list_of_cities_names, attempts, first_letter=''):
         city = input("Введите название города: ")
         if city.lower() not in list_of_cities_names:
             attempts -= 1
-            print(f"Города {city} нет в базе данных! У Вас осталось {attempts} попыток")
-        elif not first_letter:
-            list_of_cities_names.remove(city.lower())
-            return show_last_letter(city)
-        elif city.lower() in cities_with_first_letter:
+            print(f"Города {city} нет в базе данных! Попыток осталось: {attempts}")
+        elif not first_letter or city.lower() in cities_with_first_letter:
             list_of_cities_names.remove(city.lower())
             return show_last_letter(city)
         else:
@@ -44,36 +31,37 @@ def city_from_user(list_of_cities_names, attempts, first_letter=''):
         return False
 
 
-def city_from_bot(list_of_cities_names, first_letter):
+def show_city_from_bot(list_of_cities_names, first_letter):
+
     cities_with_first_letter = make_list_with_first_letter(list_of_cities_names, first_letter)
+
     if len(cities_with_first_letter) == 0:
         print("\nВы выиграли:( ")
         return False
-    else:
-        random_city = random.choice(cities_with_first_letter)
-        list_of_cities_names.remove(random_city.lower())
+
+    random_city = random.choice(cities_with_first_letter)
+    list_of_cities_names.remove(random_city.lower())
+
     return random_city.capitalize()
 
 
 def main():
-    cities_names_lower = make_letters_low(dictionary.cities_names)
+    user_attempts = 3
+    cities_names_lower = make_letters_low(cities_database.cities_names)
     last_letter = ''
-    while True:
+    for count_step in range(len(cities_database.cities_names)):
         try:
-            user_attempts = 3
-            if last_letter:
-                last_letter = city_from_user(cities_names_lower, user_attempts, last_letter)
-            else:
-                last_letter = city_from_user(cities_names_lower, user_attempts)
-            if last_letter:
-                random_city = city_from_bot(cities_names_lower, last_letter)
-                if random_city:
-                    print(f"Мой ответ: {random_city}\n")
-                    last_letter = show_last_letter(random_city)
-                else:
-                    break
-            else:
+            last_letter = handle_city_from_user(cities_names_lower, user_attempts, last_letter)
+
+            if not last_letter:
                 break
+            random_city = show_city_from_bot(cities_names_lower, last_letter)
+
+            if not random_city:
+                break
+            print(f"Мой ответ: {random_city}\n")
+            last_letter = show_last_letter(random_city)
+
         except KeyboardInterrupt:
             print("\n Пока :)")
             return False
